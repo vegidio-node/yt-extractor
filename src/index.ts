da4c29676c6112e22fe26f0cb54784b023b0ad1d
Vinicius Egidio <me@vinicius.io>
2020-07-26 04:51:03 +0200

    First deploy

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,35 @@
-import * as config from 'config'
+import * as express from 'express'
+import * as ytdl from 'ytdl-core'
+import * as mem from 'mem'
 import logger from './logger'
 
-const serverConfig = config.util.toObject(config.get('server'))
+const app = express()
 
-const msg = `Server running on ${serverConfig.host}:${serverConfig.port}`
-logger.info(msg)
+// Routes
+app.get('/video/:videoId', async (req, res) => {
+    const videoId = req.params.videoId
+    res.json(await cachedParseVideo(videoId))
+})
+
+// Parse the video information
+const parseVideo = async (videoId: string): Promise<unknown> => {
+    const info = await ytdl.getInfo(`https://www.youtube.com/watch?v=${videoId}`)
+    let format = ytdl.chooseFormat(info.formats, { quality: 'highest' });
+
+    return {
+        title: info.videoDetails.title,
+        length: Number(info.videoDetails.lengthSeconds),
+        thumbnail: info.videoDetails.thumbnail.thumbnails.pop().url,
+        stream: format.url
+    }
+}
+
+const cachedParseVideo = mem(parseVideo, {
+    maxAge: 10800   // 3 hours
+})
+
+app.use('/', (req, res) => res.send('🎥 YT Extractor'))
+
+// Starting server
+const port = process.env.PORT ?? 80
+app.listen(port, () => logger.info('🎥 YT Extractor'))
---
+++ test/foo.test.ts
@@ -0,0 +1,6 @@
+describe('Placeholder test', () =>
+{
+    test('Foo', () => {
+        expect(1).toBe(1)
+    })
+})
